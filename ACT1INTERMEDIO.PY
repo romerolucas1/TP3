db70d4734cd1e732a293d08f8a274a3d64db9aec
romerolucas1 <152455204+romerolucas1@users.noreply.github.com>
2024-06-19 10:51:19 -0300

    Create ACT1INTERMEDIO.PY

---
+++ ACT1INTERMEDIO.PY
@@ -0,0 +1,8 @@
+cristales = ["diamante", "esmeralda", "rubí", "zafiro", "esmeralda", "diamante"]
+
+cristales_unicos = set(cristales)
+
+cristales_unicos_lista = list(cristales_unicos)
+
+print("Lista única de cristales preciosos recolectados:")
+print(cristales_unicos_lista)
